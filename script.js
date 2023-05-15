0393781f6d708315d4d63882a7d7dc584dc48301
Maciej Wojciechowski <maciejwojciechowskiofc@gmail.com>
2023-05-15 19:51:15 +0200

    added editing product function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const addElement = document.getElementById("addElement-toDoList");
 const counter = document.querySelector('.container__items--toDoList--title-counter');
 const ul = document.querySelector('.container__items--toDoList--mainElement--list');
 
+let counterToDoList = 0;
+
 const toDoList = [];
 
 const addElementToList = (e) => {
@@ -33,6 +35,17 @@ const addElementToList = (e) => {
     }
 }
 
+addElement.addEventListener("keyup", addElementToList);
+
+const modalProduct = [...document.querySelectorAll('.container__items--product')];
+
+const modalButtonSave = document.getElementById("modalButtonSave");
+const modalButtonCancel = document.getElementById("modalButtonCancel");
+
+const product_name = null;
+const product_price = null;
+const product_currency = null;
+
 let product1 = {
     name: "iPhone 6s Plus 16GB",
     salePrice: 649,
@@ -53,11 +66,7 @@ let product3 = {
     price: 8000,
     currency: "PLN"
   };
-
-addElement.addEventListener("keyup", addElementToList);
-
-const modalProduct = [...document.querySelectorAll('.container__items--product')];
-
+  
 
 const modalView = () => {
     const modal = document.querySelector(".modal");
@@ -71,8 +80,9 @@ const modalView = () => {
     });
     
 }
-
 modalProduct.forEach(view => view.addEventListener('click', modalView));
+
+
 
 
 const modalProduktName = document.querySelector(".modal__input--name");
@@ -80,6 +90,14 @@ const modalProduktPrice = document.querySelector(".modal__input--price");
 const modalProduktpromoPrice = document.querySelector(".modal__input--promoPrice");
 const modalProduktCurrency = document.querySelector(".modal__select");
 
+const modalImage = document.querySelector(".modal__img-js");
+
+const promoBar = document.querySelector(".container__items--product--promoBar");
+const promoBarSecond = document.querySelector(".container__items--product--promoBarSecond");
+const promoBarThird = document.querySelector(".container__items--product--promoBarThird");
+const promoBarFinalPrice = document.querySelector(".promoBar__finalPrice");
+const promoBarFinalPriceSecond = document.querySelector(".promoBar__finalPriceSecond");
+const promoBarFinalPriceThird = document.querySelector(".promoBar__finalPriceThird");
 
 //Produkt 1
 const nameBody = document.querySelector(".product__name-js");
@@ -116,11 +134,63 @@ const currencyBodyThird = document.querySelector(".product__currency-jsThird");
 const currencyPromoBodyThird = document.querySelector(".product__currencyPromo-jsThird");
 const currencyBodyTableThird = document.querySelector(".product__currency-jsTableThird");
 
+let flag = true;
+let flag2 = true;
+let flag3 = true;
+let percent = 0;
+
+function promoPriceCounter(){
+    percent = (((100*modalProduktpromoPrice.value)/modalProduktPrice.value)-100);
+    return percent;
+}
+function promoPriceCounterReset(){
+    percent = 0;
+}
 
 modalProduct[0].addEventListener("click", () =>{
-    console.log("działa?")
-    }
-);
+    flag=false;
+    modalImage.style.backgroundImage="url(src/img1.png)"
+    const productName = document.querySelector(".modalProductName");
+    productName.innerHTML=product1.name;
+    
+    modalButtonSave.addEventListener('click', ()=>{
+        if(!flag){
+        nameBody.innerHTML = modalProduktName.value;
+        nameBodyTable.innerHTML = modalProduktName.value;
+        priceBody.innerHTML = modalProduktPrice.value;
+        priceBodyTable.innerHTML = modalProduktPrice.value;
+        promoPriceBody.innerHTML = modalProduktpromoPrice.value;
+        if(modalProduktpromoPrice.value<=0){
+            promoPriceBody.style.display="none";
+            currencyPromoBody.style.display="none";
+            priceBody.style.textDecoration="none";
+            priceBody.style.fontWeight="500";
+            priceBody.style.color="#862583";
+            currencyBody.style.textDecoration="none"
+            currencyBody.style.fontWeight="500";
+            currencyBody.style.color="#862583";
+        }else{
+            promoPriceCounterReset();
+            promoPriceCounter();
+            promoBar.classList.toggle("container__items--product--promoBar-active");
+            promoBarFinalPrice.textContent=+percent.toFixed(0) + "%";
+            promoPriceBody.style.display="inline";
+            currencyPromoBody.style.display="inline";
+            priceBody.style.textDecoration="line-through";
+            priceBody.style.fontWeight="300";
+            priceBody.style.color="#384a5c";
+            currencyBody.style.textDecoration="line-through"
+            currencyBody.style.fontWeight="300";
+            currencyBody.style.color="#384a5c";
+        }
+        promoPriceBodyTable.innerHTML = modalProduktpromoPrice.value;
+        currencyBody.innerHTML = modalProduktCurrency.value;
+        currencyPromoBody.innerHTML = modalProduktCurrency.value;
+        currencyBodyTable.innerHTML = modalProduktCurrency.value;
+        flag=true;
+        }
+    })
+});
 
 modalProduct[1].addEventListener("click", () =>{
     flag2 = false;
@@ -129,7 +199,42 @@ modalProduct[1].addEventListener("click", () =>{
     const productName = document.querySelector(".modalProductName");
     productName.innerHTML=product2.name;
     modalButtonSave.addEventListener('click', ()=>{
-        console.log("działa?")
+        if(!flag2){
+        nameBodySecond.innerHTML = modalProduktName.value;
+        nameBodyTableSecond.innerHTML = modalProduktName.value;
+        priceBodySecond.innerHTML = modalProduktPrice.value;
+        priceBodyTableSecond.innerHTML = modalProduktPrice.value;
+        promoPriceBodySecond.innerHTML = modalProduktpromoPrice.value;
+        promoPriceBodyTableSecond.innerHTML = modalProduktpromoPrice.value;
+        if(modalProduktpromoPrice.value<=0){
+            promoPriceBodySecond.style.display="none";
+            priceBodyThird.style.fontWeight="300";
+            currencyPromoBodySecond.style.display="none";
+            priceBodySecond.style.textDecoration="none";
+            priceBodySecond.style.fontWeight="500";
+            priceBodySecond.style.color="#862583";
+            currencyBodySecond.style.textDecoration="none"
+            currencyBodySecond.style.fontWeight="500";
+            currencyBodySecond.style.color="#862583";
+        }else{
+            promoPriceCounterReset();
+            promoPriceCounter();
+            promoBarSecond.classList.toggle("container__items--product--promoBar-active");
+            promoBarFinalPriceSecond.textContent= percent.toFixed(0) + "%";
+            promoPriceBodySecond.style.display="inline";
+            currencyPromoBodySecond.style.display="inline";
+            priceBodySecond.style.textDecoration="line-through";
+            priceBodySecond.style.fontWeight="300";
+            priceBodySecond.style.color="#384a5c";
+            currencyBodySecond.style.textDecoration="line-through"
+            currencyBodySecond.style.fontWeight="300";
+            currencyBodySecond.style.color="#384a5c";
+        }
+        currencyBodySecond.innerHTML = modalProduktCurrency.value;
+        currencyPromoBodySecond.innerHTML = modalProduktCurrency.value;
+        currencyBodyTableSecond.innerHTML = modalProduktCurrency.value;
+        flag2 = true;
+        }
     })
 });
 modalProduct[2].addEventListener("click", () =>{
@@ -138,6 +243,40 @@ modalProduct[2].addEventListener("click", () =>{
     const productName = document.querySelector(".modalProductName");
     productName.innerHTML=product3.name;
     modalButtonSave.addEventListener('click', ()=>{
-        console.log("działa?")
+        if(!flag3){
+        nameBodyThird.innerHTML = modalProduktName.value;
+        nameBodyTableThird.innerHTML = modalProduktName.value;
+        priceBodyThird.innerHTML = modalProduktPrice.value;
+        priceBodyTableThird.innerHTML = modalProduktPrice.value;
+        promoPriceBodyThird.innerHTML = modalProduktpromoPrice.value;
+        promoPriceBodyTableThird.innerHTML = modalProduktpromoPrice.value;
+        if(modalProduktpromoPrice.value<=0){
+            promoPriceBodyThird.style.display="none";
+            currencyPromoBodyThird.style.display="none";
+            priceBodyThird.style.textDecoration="none";
+            priceBodyThird.style.fontWeight="500";
+            priceBodyThird.style.color="#862583";
+            currencyBodyThird.style.textDecoration="none"
+            currencyBodyThird.style.fontWeight="500";
+            currencyBodyThird.style.color="#862583";
+        }else{
+            promoPriceCounterReset();
+            promoPriceCounter();
+            promoBarThird.classList.toggle("container__items--product--promoBar-active");
+            promoBarFinalPriceThird.textContent= percent.toFixed(0) + "%";
+            promoPriceBodyThird.style.display="inline";
+            currencyPromoBodyThird.style.display="inline";
+            priceBodyThird.style.textDecoration="line-through";
+            priceBodyThird.style.fontWeight="300";
+            priceBodyThird.style.color="#384a5c";
+            currencyBodyThird.style.textDecoration="line-through"
+            currencyBodyThird.style.fontWeight="300";
+            currencyBodyThird.style.color="#384a5c";
+        }
+        currencyBodyThird.innerHTML = modalProduktCurrency.value;
+        currencyPromoBodyThird.innerHTML = modalProduktCurrency.value;
+        currencyBodyTableThird.innerHTML = modalProduktCurrency.value;
+        flag3 = true;
+        }
     })
 })
